c0b716324dd4fa474039fc7e427c26fff3ae5db7
muhammadfatoni88 <75312117+muhammadfatoni88@users.noreply.github.com>
2020-12-01 19:08:27 +0700

    Add files via upload

---
+++ tugas2.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+ // Compiler version 4.0, .NET Framework 4.5
+
+
+
+
+ using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PraktikCSharp
+{
+class Program
+{
+static void Main(string[] args)
+{
+
+string namalengkap, namapanggilan, ttl, smp, alamat, tinggiberat, cita, agama,pacar, hobi;
+Console.Write("Masukkan Nama Lengkap Anda: ");
+namalengkap = Console.ReadLine();
+Console.Write("Masukkan Nama Panggilan Anda: ");
+namapanggilan = Console.ReadLine();
+Console.Write("Masukkan Tempat Tanggal Lahir Anda: ");
+ttl = Console.ReadLine();
+Console.Write("Masukkan Asal SMP/SMK/SD Anda: ");
+smp = Console.ReadLine();
+Console.Write("Masukkan Alamat Anda: ");
+alamat = Console.ReadLine();
+Console.Write("Masukkan Tinggi/Berat Anda: ");
+tinggiberat = Console.ReadLine();
+Console.Write("Masukkan Cita-Cita Anda: ");
+cita = Console.ReadLine();
+Console.Write("Masukkan Agama Anda: ");
+agama = Console.ReadLine();
+Console.Write("Masukkan Hobi Anda: ");
+hobi = Console.ReadLine();
+Console.Write("Masukkan Nama Pacar Anda: ");
+pacar = Console.ReadLine();
+Console.WriteLine("\n\nNama Lengkap :{0}",namalengkap);
+Console.WriteLine("Nama Panggilan :{0}", namapanggilan);
+Console.WriteLine("Tempat Tanggal Lahir :{0}", ttl);
+Console.WriteLine("Asal SMP/SMK/SD :{0}", smp);
+Console.WriteLine("Tinggi/Berat :{0}", tinggiberat);
+Console.WriteLine("Cita Cita :{0}", cita);
+Console.WriteLine("Agama :{0}", agama);
+Console.WriteLine("Hobi :{0}", hobi);
+Console.WriteLine("Pacar :{0}", pacar);
+
+
+
+
+
+}
+}
+}
+
